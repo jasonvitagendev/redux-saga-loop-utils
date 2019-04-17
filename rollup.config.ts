3340bbcef3b5012bf9ca51068420017325a41131
jason.c <jason.c@yxunistar.com>
2019-04-17 11:17:14 +0800

    Remove unused import

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,7 +4,6 @@ import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
-import autoExternal from 'rollup-plugin-auto-external'
 
 const pkg = require('./package.json')
 
